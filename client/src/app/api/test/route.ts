f9d1dcf58b3978081a1e08adb61f61e7c2debb54
Kartik Dwivedi <codewithme.kartik@gmail.com>
2025-02-15 15:21:13 +0530

    removed client directory

--- client/src/app/api/test/route.ts
+++
@@ -1,7 +0,0 @@
-//@ts-nocheck
-import { NextRequest, NextResponse } from "next/server";
-
-
-export async function GET(request: NextRequest, response: NextResponse) {
-  return NextResponse.json({ message: "Hello World" });
-}
